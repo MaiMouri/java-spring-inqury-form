2f83eeb6f8bf6bb18c80646b9180d435b17093f7
Mai Mouri <sfyitan@gmail.com>
2022-09-12 17:10:14 +0900

    initial commit

---
+++ src/main/java/com/example/demo/MyFirstApplicationFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstApplicationFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstApplicationFormApplication.class, args);
+	}
+
+}
